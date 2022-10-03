e0b99a6e6293c678eb5ae4c3228bbda07bfac915
n573 <ncauwet@gmail.com>
2022-10-03 08:19:40 -0600

    new-commit

---
+++ CLionProjects/lab1-2ndTry/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void readScores(int* scores, int arrLen) {
+    int in; 
+    while(in != EOF) {
+        scanf('%d', &in);
+    }
+    
+
+}
+
+void displayScores() {
+
+}
+
+void calcHistogram() {
+
+}
+
+void displayHistogram() {
+
+}
+
+void sortHistogram() {
+
+}
+
+int main() {
+    // int* scoreList;
+    int scoreList[100];
+    int numScore;
+
+
+
+    // printf("Hello, World!\n");
+    return 0;
+}
